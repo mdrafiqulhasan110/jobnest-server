62372c58bd61cd799e48fe2991abceb486de0c98
Your Namemdrafiqulhasan110 <mdrafiqulhasan110@gmail.com>
2023-11-09 12:56:58 +0600

    Database Connected

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
-const port = 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bzloljf.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
