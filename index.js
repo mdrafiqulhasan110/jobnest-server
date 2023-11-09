bc7c04529489515b1fe4c7f27181c307d0b6cba2
Your Namemdrafiqulhasan110 <mdrafiqulhasan110@gmail.com>
2023-11-09 13:12:21 +0600

    single job api created

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobs.findOne(query);
+      res.send(result);
+    });
+
     // await client.connect();
     // // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
